041ef67adf0967211fff948b549f80ca2cc68cda
spees10 <Spees.lm@gmail.com>
2022-04-13 15:53:46 +0200

    Handling Error addProduct Fun()

--- src/app/Services/products.service.ts
+++ src/app/Services/products.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IProduct } from '../ViewModels/iproduct';
 
@@ -8,8 +8,29 @@ import { IProduct } from '../ViewModels/iproduct';
   providedIn: 'root'
 })
 export class ProductsService {
+  httpOptions;
+  constructor(private httpClient: HttpClient) {
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+        //,Authorization: 'my-auth-token'
+      })
+    };
+  }
 
-  constructor(private httpClient: HttpClient) { }
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
 
   getAllProducts(): Observable<IProduct[]> {
     return this.httpClient.get<IProduct[]>(`${environment.APIURL}/products`);
@@ -23,8 +44,25 @@ export class ProductsService {
     return this.httpClient.get<IProduct>(`${environment.APIURL}/products/${prdID}`);
   }
 
-  addProduct(newPrd: IProduct) {
-    return this.httpClient.post(`${environment.APIURL}/products`, JSON.stringify(newPrd));
+  addProduct(newPrd: IProduct): Observable<IProduct> {
+    //return this.httpClient.post<IProduct>(`${environment.APIURL}/products`, JSON.stringify(newPrd), this.httpOptions);
+    // return this.httpClient.post<IProduct>(
+    //   `${environment.APIURL}/products`, JSON.stringify(newPrd), this.httpOptions
+    // ).pipe(
+    //   retry(3),
+    //   catchError((err) => {
+    //     alert(err);
+    //     return throwError(() => new Error('Post Error'))
+    //   })
+    // );
+
+    return this.httpClient.post<IProduct>(
+      `${environment.APIURL}/products`, JSON.stringify(newPrd), this.httpOptions
+    ).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+
   }
 
   updateProduct(prdID: number, UpdatePrd: IProduct) {
