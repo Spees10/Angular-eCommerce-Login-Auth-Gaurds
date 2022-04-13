1b47dfd3d0ae65e4d3b55007dfaf5e0623494001
spees10 <Spees.lm@gmail.com>
2022-04-13 13:20:06 +0200

    Product CRUD Functions

--- src/app/Services/products.service.ts
+++ src/app/Services/products.service.ts
@@ -24,14 +24,14 @@ export class ProductsService {
   }
 
   addProduct(newPrd: IProduct) {
-
+    return this.httpClient.post(`${environment.APIURL}/products`, JSON.stringify(newPrd));
   }
 
   updateProduct(prdID: number, UpdatePrd: IProduct) {
-
+    return this.httpClient.put(`${environment.APIURL}/products/${prdID}`, JSON.stringify(UpdatePrd));
   }
 
   deleteProduct(prdID: number) {
-    //return this.httpClient.get<IProduct[]>(`${environment.APIURL}/Products/${prdID}`);
+    return this.httpClient.delete(`${environment.APIURL}/products/${prdID}`);
   }
 }
